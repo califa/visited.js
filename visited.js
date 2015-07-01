9c69f9565a7095b62414648fd087e95562a5e3de
Joel Califa <joel@digitalocean.com>
2015-06-30 23:35:54 -0700

    add initial script

---
+++ visited.js
@@ -0,0 +1,10 @@
+localStorage.setItem('visited-'+window.location.pathname,true);
+var links = document.getElementsByTagName('a');
+for (i=0;i<links.length;i++) {   
+  var link = links[i];
+  if (link.host == window.location.host 
+  && localStorage.getItem('visited-'+link.pathname)
+  || localStorage.getItem('visited-'+link.pathname + '/')) {
+    link.dataset.visited = true;
+  }
+}
